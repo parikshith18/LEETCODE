7e7c2cd3ec77b0781d24c32bbb1ad35f3ba57e2a
parikshith18 <167424710+parikshith18@users.noreply.github.com>
2026-08-19 01:20:01 +0530

    Time: 0 ms (100%), Space: 10 MB (70.83%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.size(), m = s2.size();
+        if(n > m)
+            return false;
+
+        vector<int> need(26, 0), window(26, 0);
+        for(char c: s1)
+            need[c - 'a']++;
+        
+        int left = 0;
+        for(int right = 0; right < m; right++){
+            window[s2[right] - 'a']++;
+            if(right - left + 1 > n){
+                window[s2[left] - 'a']--;
+                left++;
+            }
+            if(window == need)
+                return true;
+        }
+        return false;
+    }
+};
